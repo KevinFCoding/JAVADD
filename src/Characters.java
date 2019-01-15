2f9438224e1c4689261c5d65c31c17114455ed41
Quentin <quentin.valbeny@edu.itescia.fr>
2019-01-15 11:40:40 +0100

    Création map

--- src/Characters.java
+++ src/Characters.java
@@ -1,15 +1,27 @@
+import java.util.Map;
+
 public class Characters {
 
     String name;
+    int life;
     int lvl;
+
+  /*  int exp;
     int power;
     int dexterity;
     int constitution;
     int inteligency;
     int wisdom;
     int charisma;
-    int life;
+  */
+  Map<String, Integer> carac;
+  carac.put("Strength",10);
+  carac.put();
+  carac.put();
+  carac.put();
+  carac.put();
+  carac.put();
 
-    private
+
 }
 
