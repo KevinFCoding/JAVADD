6df3f1f9d48260bf2880ead3ad40ae27282f9b3f
Unknown <quentin.valbeny@edu.itescia.fr>
2019-01-15 11:31:25 +0100

    Ajout variable

---
+++ src/Characters.java
@@ -0,0 +1,15 @@
+public class Characters {
+
+    String name;
+    int lvl;
+    int power;
+    int dexterity;
+    int constitution;
+    int inteligency;
+    int wisdom;
+    int charisma;
+    int life;
+
+    private
+}
+
