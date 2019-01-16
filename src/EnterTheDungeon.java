2870bc59928e4253bf8e013c41635cccf8e3ee6e
Unknown <anthony.guiock@edu.itescia.fr>
2019-01-16 15:23:13 +0100

    correction merge

--- src/EnterTheDungeon.java
+++ src/EnterTheDungeon.java
@@ -6,23 +6,16 @@ public class EnterTheDungeon {
 
 	public static void main(String[] args) {
 		
-		/*Wizard Kevin = new Wizard("Orc", "Forgeron");
+		Wizard kevin = new Wizard("Orc", "Forgeron");
 		
-		System.out.println("For : " + Kevin.getCarac("Strength"));
-		System.out.println("Dex : " +Kevin.getCarac("Dexterity"));
-		System.out.println("Con : " +Kevin.getCarac("Constitution"));
-		System.out.println("Int : " +Kevin.getCarac("Intelligence"));
-		System.out.println("Sag : " +Kevin.getCarac("Wisdom"));
-		System.out.println("Cha : " +Kevin.getCarac("Charisma"));
-
-		/*Ranger Bob = new Ranger("Human", "Hunter");
-
-		System.out.println("For : " +Bob.getCarac("Strength"));
-		System.out.println("Dex : " +Bob.getCarac("Dexterity"));
-		System.out.println("Con : " +Bob.getCarac("Constitution"));
-		System.out.println("Int : " +Bob.getCarac("Intelligence"));
-		System.out.println("Sag : " +Bob.getCarac("Wisdom"));
-		System.out.println("Cha : " +Bob.getCarac("Charisma"));
+		Ranger bob = new Ranger("Humain", "Hunter");
+		
+		for(String key : Characters.stats){
+			System.out.println(key + " : " + bob.getAllCarac().get(key));
+		}
+		for(String key : Characters.stats){
+			System.out.println(key + " : " + kevin.getAllCarac().get(key));
+		}
 	}
 
 }
